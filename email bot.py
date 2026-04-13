22b5a4c49fa1ff6835a765e12a192263d3141cb0
PRITHVIRAJ SANJEEVAN <prithvisanjeevan20@gmail.com>
2026-04-13 13:33:37 +0530

    Add files via upload

--- email bot.py
+++ email bot.py
@@ -43,6 +43,8 @@ def mail(f_name,l_name,mailname,b_year,initial,r_year):
         with open("usernmpwd.txt","w") as f:
             for i in emails:
                 f.write(i+"\n")
+        
+
 
 
 
@@ -71,10 +73,7 @@ def insta(f_name,l_name,b_year,initial):
         with open("usernmpwd.txt","w") as f:
             for i in insta_uname:
                 f.write(i+"\n")
-    else:
-        print()
-        print("Thank you for using Mail Generator😺")
-        print("Have a great day 😊")
+
 
 def twitter(f_name,l_name,b_year,initial,r_year):
     x=random.randint(99,999)
@@ -90,6 +89,7 @@ def twitter(f_name,l_name,b_year,initial,r_year):
     for i in twitter_name:
         print("➤  ", i)
 
+    print()
 
     twit_pwd=input("GENERATE PASSWORD:[YES/NO]     ").lower()
     if twit_pwd=="yes":
@@ -101,115 +101,119 @@ def twitter(f_name,l_name,b_year,initial,r_year):
         with open("usernmpwd.txt","w") as f:
             for i in twitter_name:
                 f.write(i+"\n")
-#GETTING USER INPUT
-while True:
-    def main():
+    
 
-        print("====CHOOSE DOMAIN====")
-        print("1.GMAIL")
-        print("2.OUTLOOK")
-        print("3.I-CLOUD")
-        print("4.YAHOO")
-        print("5.REDIFF")
-        print("6.HOTMAIL")
-        print("7.LIVEMAIL")
-        print("8.COMPANY MAIL")
-        print("9.COLLEGE MAIL")
-        print("10.OTHER--ENTER DOMAIN NAME")
-        print("11.INSTAGRAM-USER NAME")
-        print("12.TWITTER-X")
-        print("CHOOSE BY NUMBER:")
+def main():
 
-        domain_lst=[1,2,3,4,5,6,7,8,9,10,11,12]
+    print("====CHOOSE DOMAIN====")
+    print("1.GMAIL")
+    print("2.OUTLOOK")
+    print("3.I-CLOUD")
+    print("4.YAHOO")
+    print("5.REDIFF")
+    print("6.HOTMAIL")
+    print("7.LIVEMAIL")
+    print("8.COMPANY MAIL")
+    print("9.COLLEGE MAIL")
+    print("10.OTHER--ENTER DOMAIN NAME")
+    print("11.INSTAGRAM-USER NAME")
+    print("12.TWITTER-X")
+    print("CHOOSE BY NUMBER:")
 
+    domain_lst=[1,2,3,4,5,6,7,8,9,10,11,12]
 
+    try:
         domain=int(input("DOMAIN NUMBER:"))
         while domain not in domain_lst:
             print("INVALID--CHOICE")
             domain=int(input("DOMAIN NUMBER:"))
+    except ValueError:
+        print("ENTER NUMBER ONLY")
 
+    f_name=input("FIRST NAME:").lower()
+    while f_name.isdigit():
+        print("INVALID NAME:")
         f_name=input("FIRST NAME:").lower()
-        while f_name.isdigit():
-            print("INVALID NAME:")
-            f_name=input("FIRST NAME:").lower()
 
 
+    l_name=input("LAST NAME:").lower()
+    while l_name.isdigit():
+        print("INVALID NAME:")
         l_name=input("LAST NAME:").lower()
-        while l_name.isdigit():
-            print("INVALID NAME:")
-            l_name=input("LAST NAME:").lower()
 
+    initial=input("INITIAL:").lower()
+    while initial.isdigit():
+        print("Invalid-INITIAL")
         initial=input("INITIAL:").lower()
-        while initial.isdigit():
-            print("Invalid-INITIAL")
-            initial=input("INITIAL:").lower()
 
-        b_year=input("BITH YEAR:")
-        while not (b_year.isdigit() and len(b_year) == 4):
-            print("INVALID INPUT ")
-            b_year=input("BITH YEAR:")
+    b_year=input("BIRTH YEAR:")
+    while not (b_year.isdigit() and len(b_year) == 4):
+        print("INVALID INPUT ")
+        b_year=input("BIRTH YEAR:")
 
-        r_year=b_year[::-1]
+    r_year=b_year[::-1]
 
 
-        #ADDRESSING FOR RIGHT MAIL
+    #ADDRESSING FOR RIGHT MAIL
 
-        if domain==1:
-            mailname="gmail"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    if domain==1:
+        mailname="gmail"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==2:
-            mailname="outlook"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==2:
+        mailname="outlook"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==3:
-            mailname="icloud"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==3:
+        mailname="icloud"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==4:
-            mailname="yahoo"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==4:
+        mailname="yahoo"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==5:
-            mailname="rediff"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==5:
+        mailname="rediff"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==6:
-            mailname="hotmail"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==6:
+        mailname="hotmail"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==7:
-            mailname="livemail"
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==7:
+        mailname="livemail"
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==8:
-            mailname=input("COMPANY NAME:")
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==8:
+        mailname=input("COMPANY NAME:")
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==9:
-            mailname=input("COLLEGE NAME:")
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==9:
+        mailname=input("COLLEGE NAME:")
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==10:
-            mailname=input("DOMAIN NAME:")
-            mail(f_name,l_name,mailname,b_year,initial,r_year)
+    elif domain==10:
+        mailname=input("DOMAIN NAME:")
+        mail(f_name,l_name,mailname,b_year,initial,r_year)
 
-        elif domain==11:
-            insta(f_name,l_name,b_year,initial)
+    elif domain==11:
+        insta(f_name,l_name,b_year,initial)
 
-        elif domain==12:
-            twitter(f_name,l_name,b_year,initial,r_year)
+    elif domain==12:
+        twitter(f_name,l_name,b_year,initial,r_year)
 
 
-    main()
+main()
 
-    user_inp=input("DO YOU WANT TO EXIT  YES OR NO ").lower()
-    if user_inp!="yes":
-        break
-
+while True:
+    
+    user_inp = input("DO YOU WANT TO GENERATE USERNAME AGAIN [YES/NO]: ").lower()
+    if user_inp == "yes":
+        main()
     else:
         print()
-        print("Thank you for using  Mail Generator😺")
+        print("Thank you for using username bot 😺")
         print("Have a great day 😊")
+        break 
 
 
